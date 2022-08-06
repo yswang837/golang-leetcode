94d100f1a88c76b56d4a8abc10f4f65b7d59900e
wys <yswang837@gmail.com>
2022-08-06 14:43:07 +0800

    后序遍历

---
+++ 145/main.go
@@ -0,0 +1,22 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func postorderTraversal(root *TreeNode) []int {
+	var res []int
+	if root == nil {
+		return res
+	}
+	res = append(res, postorderTraversal(root.Left)...)
+	res = append(res, postorderTraversal(root.Right)...)
+	res = append(res, root.Val)
+	return res
+}
+
+func main() {
+
+}
