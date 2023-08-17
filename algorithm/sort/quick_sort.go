e0f7eb6ebbe7f183c7f0c22c92095658cc1c3e0b
小钻风 <80513574+yswang837@users.noreply.github.com>
2023-08-17 22:03:45 +0800

    1

--- algorithm/sort/quick_sort.go
+++ algorithm/sort/quick_sort.go
@@ -15,34 +15,27 @@ func QuickSort(nums []int) []int {
 
 }
 
-// 原地交换，所以传入交换索引
 func quickSort(nums []int, start, end int) {
 	if start < end {
-		// 分治法：divide
-		pivot := partition(nums, start, end)
-		quickSort(nums, 0, pivot-1)
-		quickSort(nums, pivot+1, end)
+		povit := partition(nums, start, end)
+		quickSort(nums, start, povit-1)
+		quickSort(nums, povit+1, end)
 	}
 }
 
-// 分区
 func partition(nums []int, start, end int) int {
-	// 选取最后一个元素作为基准pivot
-	p := nums[end]
+	povit := nums[end]
 	i := start
-	// 最后一个值就是基准所以不用比较
 	for j := start; j < end; j++ {
-		if nums[j] < p {
+		if nums[j] < povit {
 			swap(nums, i, j)
 			i++
 		}
 	}
-	// 把基准值换到中间
 	swap(nums, i, end)
 	return i
 }
 
-// 交换两个元素
 func swap(nums []int, i, j int) {
 	nums[i], nums[j] = nums[j], nums[i]
 }
