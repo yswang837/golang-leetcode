20c47d6df625e91a5a6f194a9dd1a2d4a35c599b
小钻风 <80513574+yswang837@users.noreply.github.com>
2023-08-18 09:03:12 +0800

    1

--- algorithm/search/binary_search.go
+++ algorithm/search/binary_search.go
@@ -3,21 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Search([]int{2, 3, 3, 4, 4, 5, 5, 5, 6, 6, 17, 43, 45, 65, 67}, 17))
+	fmt.Println(Search([]int{2, 3, 3, 4, 4, 5, 5, 5, 6, 6, 17, 43, 45, 65, 67}, 4))
 }
 
 // Search 二分搜索最常用模板
 func Search(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1
+	}
 	low, high := 0, len(nums)-1
 	for low <= high {
-		mid := (high + low) / 2
-		if nums[mid] == target {
-			return mid
-		} else if nums[mid] > target {
-			high = mid - 1
-		} else if nums[mid] < target {
-			low = mid + 1
+		middle := (low + high) / 2
+		if nums[middle] == target {
+			return middle
+		} else if nums[middle] < target {
+			low = middle + 1
+		} else {
+			high = middle - 1
 		}
 	}
-	return -1
+	return -2
 }
