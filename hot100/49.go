37dfcb716dbbd3d5b40f69c0f9a998c2a74ec4bf
小钻风 <80513574+yswang837@users.noreply.github.com>
2023-09-04 15:06:52 +0800

    1

---
+++ hot100/49.go
@@ -0,0 +1,30 @@
+//https://leetcode.cn/problems/group-anagrams/description/?envType=study-plan-v2&envId=top-100-liked
+
+import "sort"
+
+func groupAnagrams(strs []string) [][]string {
+	ret := [][]string{}
+	if len(strs) == 0 {
+		return ret
+	}
+	if len(strs) == 1 {
+		ret = append(ret,strs)
+		return ret
+	}
+	m := map[string][]string{}
+	var b []byte
+	for _,str := range strs {
+		b = []byte(str)
+		sort.Slice(b, func(i, j int) bool {
+			return b[i] < b[j]
+		})
+		key := string(b)
+		itemSlice := append(m[key],str)
+		m[key] = itemSlice
+
+	}
+	for _,val := range m {
+		ret = append(ret,val)
+	}
+	return ret
+}
