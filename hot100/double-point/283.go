743c13c72c87b5312a591f050c1d953418608c11
小钻风 <80513574+yswang837@users.noreply.github.com>
2023-11-13 16:07:42 +0800

    1

--- hot100/double-point/283.go
+++ hot100/double-point/283.go
@@ -2,6 +2,15 @@ package double_point
 
 //https://leetcode.cn/problems/move-zeroes/?envType=study-plan-v2&envId=top-100-liked
 
-func main() {
-
+func moveZeroes(nums []int) {
+	slow, fast, length := 0, 0, len(nums)
+	for fast < length {
+		if nums[fast] != 0 {
+			nums[slow], nums[fast] = nums[fast], nums[slow]
+			slow++
+		}
+		fast++
+	}
 }
+
+// [0,1,0,3,12]
