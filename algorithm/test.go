f002e7f01445dd0bee3659ce9019284e1d8d2ee0
小钻风 <80513574+yswang837@users.noreply.github.com>
2023-11-30 16:11:25 +0800

    1

--- algorithm/test.go
+++ algorithm/test.go
@@ -1,38 +1,104 @@
 package main
 
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
 
-type record struct {
-	res   int
-	depth int
-}
-
-func maxDepth(root *TreeNode) int {
-	r := &record{}
-	traverse(root, r)
-	return r.res
-}
-
-func traverse(root *TreeNode, r *record) {
-	if root == nil {
-		return
+func main() {
+	game := NewGame()
+	// 10个运动员参加比赛
+	for i := 1; i <= 10; i++ {
+		runner := NewRunner(i)
+		game.SetRunner(runner)
 	}
-	r.depth++
-	if root.Left == nil && root.Right == nil {
-		r.res = maxA(r.res, r.depth)
-	}
-	traverse(root.Left, r)
-	traverse(root.Right, r)
-	r.depth--
+	// 起跑线前准备
+	game.Prepare()
+	// 发令枪响
+	game.Start()
+	// 等待运动员跑完
+	game.wg.Wait()
 }
 
-func maxA(i, j int) int {
-	if i > j {
-		return i
+type Runner struct {
+	id int
+}
+
+func (r *Runner) Speed() float64 {
+	return 5 + rand.Float64()*10
+}
+
+func NewRunner(id int) *Runner {
+	return &Runner{id: id}
+}
+
+type Game struct {
+	length  int // 赛道
+	runners []*Runner
+
+	wg    sync.WaitGroup // 控制运动员是否全部跑完
+	start chan struct{}  // 发令枪
+	end   chan int       // 终点
+}
+
+func NewGame() *Game {
+	return &Game{
+		length:  100,
+		runners: make([]*Runner, 0),
+		start:   make(chan struct{}),
+		end:     make(chan int),
 	}
-	return j
+}
+
+func (g *Game) SetLength(length int) {
+	g.length = length
+}
+
+func (g *Game) SetRunner(runner *Runner) {
+	g.runners = append(g.runners, runner)
+}
+
+func (g *Game) Prepare() {
+	for _, runner := range g.runners {
+		g.wg.Add(1)
+		go func(r *Runner) {
+			defer g.wg.Done()
+			for {
+				select {
+				case <-g.start:
+					time.Sleep(time.Duration(float64(g.length)/r.Speed()) * time.Second)
+					g.end <- r.id
+					return
+				}
+			}
+
+		}(runner)
+	}
+	// 统计成绩
+	g.wg.Add(1)
+	go func() {
+		defer g.wg.Done()
+		// 发令枪开始后才统计成绩
+		for {
+			select {
+			case <-g.start: // 比赛开始，开始计时
+				now := time.Now()
+				top := 1
+				for id := range g.end {
+					spend := time.Now().Sub(now).Seconds()
+					fmt.Printf("第%d名: \t运动员编号：%d\t花费时间：%f\n", top, id, spend)
+					top++
+					if top == len(g.runners)+1 {
+						return
+					}
+				}
+			}
+		}
+	}()
+}
+
+func (g *Game) Start() {
+	close(g.start)
 }
---
+++ hot100/double-point/42.go
@@ -0,0 +1,5 @@
+package double_point
+
+func main() {
+
+}
