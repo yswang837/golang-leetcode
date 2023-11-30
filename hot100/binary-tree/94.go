78f121ca5cdda60a6f75827e2950f7b30ab411f4
小钻风 <80513574+yswang837@users.noreply.github.com>
2023-11-30 16:51:21 +0800

    binary-tree

--- hot100/binary-tree/94.go
+++ hot100/binary-tree/94.go
@@ -1,5 +1,19 @@
 package binary_tree
 
-func main() {
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
 
+func inorderTraversal(root *TreeNode) []int {
+	var res []int
+	if root == nil {
+		return res
+	}
+	res = append(res, inorderTraversal(root.Left)...)
+	res = append(res, root.Val)
+	res = append(res, inorderTraversal(root.Right)...)
+
+	return res
 }
--- hot100/dynamic-programming/70.go
+++ hot100/dynamic-programming/70.go
@@ -1,5 +1,21 @@
 package dynamic_programming
 
-func main() {
-
+func climbStairs(n int) int {
+	if n < 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	if n == 2 {
+		return 2
+	}
+	res := make([]int, n+1)
+	res[0] = 0
+	res[1] = 1
+	res[2] = 2
+	for i := 3; i <= n; i++ {
+		res[i] = res[i-1] + res[i-2]
+	}
+	return res[n]
 }
