24dd165d5a935dcb7dad9c0d461d51e88181ede9
小钻风 <80513574+yswang837@users.noreply.github.com>
2023-11-30 16:55:19 +0800

    优化结构

--- hot100/binary-tree/102.go
+++ hot100/binary-tree/102.go
@@ -1,5 +1,33 @@
 package binary_tree
 
+func levelOrder(root *TreeNode) [][]int {
+	var res [][]int
+	if root == nil {
+		return res
+	}
+	var q []*TreeNode
+	q = append(q, root)
+	for len(q) > 0 {
+		length := len(q)
+		var level []int
+		for i := 0; i < length; i++ {
+			node := q[0]
+			q = q[1:]
+			level = append(level, node.Val)
+			if node.Left != nil {
+				q = append(q, node.Left)
+			}
+			if node.Right != nil {
+				q = append(q, node.Right)
+			}
+		}
+		if len(level) > 0 {
+			res = append(res, level)
+		}
+	}
+	return res
+}
+
 func main() {
 
 }
--- hot100/link-list/206.go
+++ hot100/link-list/206.go
@@ -1,5 +1,26 @@
 package link_list
 
-func main() {
-
+type ListNode struct {
+	Val  int
+	Next *ListNode
 }
+
+func reverseList(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	var pre *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = next
+	}
+	return pre
+}
+
+//  h
+//       p c n
+// node<-1 2->3->4->5
+// 1->2->3->4->5
