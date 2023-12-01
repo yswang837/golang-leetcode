5046780e6ba68451190854bfa11f829c94ed0792
小钻风 <80513574+yswang837@users.noreply.github.com>
2023-12-01 17:03:23 +0800

    二叉树的直径

--- hot-100/binary-tree/543.go
+++ hot-100/binary-tree/543.go
@@ -1,5 +1,24 @@
 package binary_tree
 
-func main() {
+func diameterOfBinaryTree(root *TreeNode) int {
+	maxDiameter := 0
+	maxDepth(root, &maxDiameter)
+	return maxDiameter
+}
 
+func maxDepth(root *TreeNode, maxDiameter *int) int {
+	if root == nil {
+		return 0
+	}
+	leftDepth := maxDepth(root.Left, maxDiameter)
+	rightDepth := maxDepth(root.Right, maxDiameter)
+	curDepth := leftDepth + rightDepth
+	if curDepth > *maxDiameter {
+		*maxDiameter = curDepth
+	}
+	if leftDepth >= rightDepth {
+		return leftDepth + 1
+	} else {
+		return rightDepth + 1
+	}
 }
