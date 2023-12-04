e65171e41d6b3f06056251687f2da85e49ad492f
小钻风 <80513574+yswang837@users.noreply.github.com>
2023-12-04 16:32:11 +0800

    二叉树的右视图

--- hot-100/binary-tree/199.go
+++ hot-100/binary-tree/199.go
@@ -1,5 +1,21 @@
 package binary_tree
 
-func main() {
-
+func rightSideView(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	var ret []int
+	var f func(node *TreeNode, depth int)
+	f = func(node *TreeNode, depth int) {
+		if node == nil {
+			return
+		}
+		if depth > len(ret) {
+			ret = append(ret, node.Val)
+		}
+		f(node.Right, depth+1)
+		f(node.Left, depth+1)
+	}
+	f(root, 1)
+	return ret
 }
