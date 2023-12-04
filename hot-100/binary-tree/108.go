a96aa442899cc7b65d8571092d23852fb4ff91ef
小钻风 <80513574+yswang837@users.noreply.github.com>
2023-12-04 10:41:15 +0800

    将有序数组转为BST，二叉搜索树

--- hot-100/binary-tree/108.go
+++ hot-100/binary-tree/108.go
@@ -1,5 +1,19 @@
 package binary_tree
 
-func main() {
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	return traverse(nums, 0, len(nums)-1)
+}
 
+func traverse(nums []int, left int, right int) *TreeNode {
+	if left > right {
+		return nil
+	}
+	middle := (left + right) / 2
+	root := &TreeNode{Val: nums[middle]}
+	root.Left = traverse(nums, left, middle-1)
+	root.Right = traverse(nums, middle+1, right)
+	return root
 }
