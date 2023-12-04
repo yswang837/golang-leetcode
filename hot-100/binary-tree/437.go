3dd9f538b8f55437c9e6b44e78d5750c582d97f7
小钻风 <80513574+yswang837@users.noreply.github.com>
2023-12-04 18:55:40 +0800

    二叉树路径总和

--- hot-100/binary-tree/437.go
+++ hot-100/binary-tree/437.go
@@ -1,5 +1,20 @@
 package binary_tree
 
-func main() {
-
+func pathSum(root *TreeNode, targetSum int) int {
+	presumCntMap := map[int]int{0: 1}
+	var f func(n *TreeNode, curSum int) (ans int)
+	f = func(n *TreeNode, curSum int) (ans int) {
+		if n == nil {
+			return
+		}
+		curSum += n.Val
+		if cnt, ok := presumCntMap[curSum-targetSum]; ok {
+			ans += cnt
+		}
+		presumCntMap[curSum]++
+		ans = ans + f(n.Left, curSum) + f(n.Right, curSum)
+		presumCntMap[curSum]--
+		return
+	}
+	return f(root, 0)
 }
