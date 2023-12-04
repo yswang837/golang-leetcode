a679429126a47370af633a8aff58840c8482ff89
小钻风 <80513574+yswang837@users.noreply.github.com>
2023-12-04 11:49:04 +0800

    验证二叉搜索树

--- hot-100/binary-tree/98.go
+++ hot-100/binary-tree/98.go
@@ -1,5 +1,15 @@
 package binary_tree
 
-func main() {
+import "math"
 
+func dfs(node *TreeNode, left, right int) bool {
+	if node == nil {
+		return true
+	}
+	x := node.Val
+	return left < x && x < right && dfs(node.Left, left, x) && dfs(node.Right, x, right)
+}
+
+func isValidBST(root *TreeNode) bool {
+	return dfs(root, math.MinInt, math.MaxInt)
 }
