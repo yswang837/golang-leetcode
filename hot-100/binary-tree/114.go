421746c1b3c4a19ac50b5e8ecfebca9891111b63
小钻风 <80513574+yswang837@users.noreply.github.com>
2023-12-04 16:46:35 +0800

    二叉树展开成链表

--- hot-100/binary-tree/114.go
+++ hot-100/binary-tree/114.go
@@ -1,5 +1,22 @@
 package binary_tree
 
-func main() {
+// 动态规划，划分子问题，传递一棵树，返回一个链表。只需要考虑当前root节点应该怎么做，递归函数会自动遍历出所有节点。
+func flatten(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	flatten(root.Left)
+	flatten(root.Right)
 
+	left := root.Left
+	right := root.Right
+
+	root.Left = nil
+	root.Right = left
+
+	node := root
+	for node.Right != nil {
+		node = node.Right
+	}
+	node.Right = right
 }
