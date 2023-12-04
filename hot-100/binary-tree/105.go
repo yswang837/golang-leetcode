6ed221ebcd80e80c2129267737527270014a5f32
小钻风 <80513574+yswang837@users.noreply.github.com>
2023-12-04 17:21:20 +0800

    根据前序中序，求后序

--- hot-100/binary-tree/105.go
+++ hot-100/binary-tree/105.go
@@ -1,5 +1,19 @@
 package binary_tree
 
-func main() {
-
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 || len(inorder) == 0 {
+		return nil
+	}
+	var root int
+	for k, v := range inorder {
+		if v == preorder[0] {
+			root = k
+			break
+		}
+	}
+	return &TreeNode{
+		Val:   preorder[0],
+		Left:  buildTree(preorder[1:root+1], inorder[0:root]),
+		Right: buildTree(preorder[root+1:], inorder[root+1:]),
+	}
 }
