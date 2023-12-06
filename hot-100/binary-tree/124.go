32245295be110d02a0ce407875209c36f9c3ea23
小钻风 <80513574+yswang837@users.noreply.github.com>
2023-12-06 15:14:38 +0800

    树的最大路径和

--- hot-100/binary-tree/124.go
+++ hot-100/binary-tree/124.go
@@ -1,5 +1,27 @@
 package binary_tree
 
-func main() {
+import "math"
 
+func maxPathSum(root *TreeNode) int {
+	maxSum := math.MinInt32
+	var maxGain func(node *TreeNode) int
+	maxGain = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+		leftGain := maxInt(maxGain(node.Left), 0)
+		rightGain := maxInt(maxGain(node.Right), 0)
+		currentSum := node.Val + leftGain + rightGain
+		maxSum = maxInt(maxSum, currentSum)
+		return node.Val + max(leftGain, rightGain)
+	}
+	maxGain(root)
+	return maxSum
+}
+
+func maxInt(i, j int) int {
+	if i >= j {
+		return i
+	}
+	return j
 }
--- hot-100/binary-tree/236.go
+++ hot-100/binary-tree/236.go
@@ -1,5 +1,16 @@
 package binary_tree
 
-func main() {
-
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil || root == p || root == q {
+		return root
+	}
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left != nil && right != nil {
+		return root
+	}
+	if left != nil {
+		return left
+	}
+	return right
 }
